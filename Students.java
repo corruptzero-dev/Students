7e3262979ffae956f43d15467f8b1bd489309d35
corruptzero-dev <74492746+corruptzero-dev@users.noreply.github.com>
2021-11-18 11:53:32 +0300

    Update Students.java

--- Students.java
+++ Students.java
@@ -30,16 +30,20 @@ public class Students {
             for (Map.Entry<String, Integer> ent : set) {
                 if(ent.getValue()==-1){
                     int mark = -1;
-                    while(!(0<mark && mark<=5)){
+                    boolean isPrint = false;
                         try{
-                            System.out.printf("Введите оценку для студента %s: ", ent.getKey());
-                            mark = scanner.nextInt();
-                            studentsMarks.put(ent.getKey(), mark);
+                            while(!(0<mark && mark<=5) && !isPrint) {
+                                System.out.printf("Введите оценку для студента %s: ", ent.getKey());
+                                mark = scanner.nextInt();
+                                studentsMarks.put(ent.getKey(), mark);
+                            }
                         } catch (Exception e){
+                            if (scanner.next().equals("print")){
+                                isPrint = true;
+                            }
                             System.out.println("Неверный ввод!");
                             scanner.nextLine();
                         }
-                    }
                 }
             }
         }
