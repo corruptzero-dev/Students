d958c8bd20fb83fef323ebd5de219555e35583e5
corruptzero-dev <74492746+corruptzero-dev@users.noreply.github.com>
2021-11-19 23:00:04 +0300

    final changes

--- Students.java
+++ Students.java
@@ -1,4 +1,4 @@
-package com.company;
+package task;
 
 import java.util.*;
 
@@ -19,35 +19,39 @@ public class Students {
     };
 
 
-    public void fill(){
-        Collections.shuffle(Collections.singletonList(names));
-        Arrays.stream(names).forEach((Objects.requireNonNull(elem -> studentsMarks.put(elem, -1))));
-
+    public void fill() {
+        Arrays.stream(names).forEach((Objects.requireNonNull(elem -> studentsMarks.put(elem, 0))));
     }
-    public void putMark(){
+
+    public void putMark() {
+        String mark = "";
         Set<Map.Entry<String, Integer>> set = studentsMarks.entrySet();
-        while (studentsMarks.containsValue(-1)){
-            for (Map.Entry<String, Integer> ent : set) {
-                if(ent.getValue()==-1){
-                    int mark = -1;
-                    boolean isPrint = false;
-                        try{
-                            while(!(0<mark && mark<=5) && !isPrint) {
-                                System.out.printf("Введите оценку для студента %s: ", ent.getKey());
-                                mark = scanner.nextInt();
-                                studentsMarks.put(ent.getKey(), mark);
-                            }
-                        } catch (Exception e){
-                            if (scanner.next().equals("print")){
-                                isPrint = true;
-                            }
+        while (studentsMarks.containsValue(0)) {
+            try {
+                for (Map.Entry<String, Integer> ent : set) {
+                    if (ent.getValue() == 0) {
+                        System.out.printf("Введите оценку для студента %s: ", ent.getKey());
+                        mark = scanner.next();
+                        if (mark.equals("print")) {
+                            break;
+                        }
+                        if ((0 < Integer.parseInt(mark) && Integer.parseInt(mark) <= 5)) {
+                            studentsMarks.put(ent.getKey(), Integer.parseInt(mark));
+                        } else {
                             System.out.println("Неверный ввод!");
                             scanner.nextLine();
                         }
+                    }
                 }
+                if (mark.equals("print")) {
+                    break;
+                }
+            } catch (Exception e) {
+                System.out.println("Неверный ввод!");
+                scanner.nextLine();
             }
         }
         System.out.println("___Результаты___");
-        studentsMarks.forEach((key, value) -> System.out.println(key + ": " + value));
+        studentsMarks.forEach((key, value) -> System.out.println(value == 0 ? key + ": нет оценки" : key + ": " + value));
     }
 }
