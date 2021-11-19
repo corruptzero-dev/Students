d78fafedaecc33ef2625f61615b5038e079e7322
corruptzero-dev <74492746+corruptzero-dev@users.noreply.github.com>
2021-11-19 23:32:39 +0300

    final final changes

--- Students.java
+++ Students.java
@@ -26,32 +26,29 @@ public class Students {
     public void putMark() {
         String mark = "";
         Set<Map.Entry<String, Integer>> set = studentsMarks.entrySet();
-        while (studentsMarks.containsValue(0)) {
-            try {
-                for (Map.Entry<String, Integer> ent : set) {
-                    if (ent.getValue() == 0) {
-                        System.out.printf("Введите оценку для студента %s: ", ent.getKey());
-                        mark = scanner.next();
-                        if (mark.equals("print")) {
-                            break;
-                        }
-                        if ((0 < Integer.parseInt(mark) && Integer.parseInt(mark) <= 5)) {
-                            studentsMarks.put(ent.getKey(), Integer.parseInt(mark));
-                        } else {
-                            System.out.println("Неверный ввод!");
-                            scanner.nextLine();
+            for (Map.Entry<String, Integer> ent : set) {
+                try {
+                    if (studentsMarks.containsValue(0)) {
+                        if (ent.getValue() == 0) {
+                            System.out.printf("Введите оценку для студента %s: ", ent.getKey());
+                            mark = scanner.next();
+                            if (mark.equals("print")) {
+                                break;
+                            }
+                            if ((0 < Integer.parseInt(mark) && Integer.parseInt(mark) <= 5)) {
+                                studentsMarks.put(ent.getKey(), Integer.parseInt(mark));
+                            } else {
+                                System.out.println("Неверный ввод!");
+                                putMark();
+                            }
                         }
                     }
+                } catch (Exception e) {
+                    System.out.println("Неверный ввод!");
+                    putMark();
                 }
-                if (mark.equals("print")) {
-                    break;
-                }
-            } catch (Exception e) {
-                System.out.println("Неверный ввод!");
-                scanner.nextLine();
             }
+            System.out.println("___Результаты___");
+            studentsMarks.forEach((key, value) -> System.out.println(value == 0 ? key + ": нет оценки" : key + ": " + value));
         }
-        System.out.println("___Результаты___");
-        studentsMarks.forEach((key, value) -> System.out.println(value == 0 ? key + ": нет оценки" : key + ": " + value));
     }
-}
